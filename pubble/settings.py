2690f76a22af9ee982048e1f24c2ce34e6d796d5
oduagbondestiny <oduagondestiny@gmail.com>
2025-09-17 23:49:54 +0100

    Prepare Django app for Render

--- pubble/settings.py
+++ pubble/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@o-o0etel4gr=97onj13p&xzbdvl^j*j*2z(+%$58r1$2vov5)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 LOGIN_URL = '/LOGIN'
 LOGIN_REDIRECT_URL = '/'
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
